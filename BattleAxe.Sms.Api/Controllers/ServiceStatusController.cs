0bcdb62df1fcc3ac8545125b543ee53c8edd1a38
Warwick858 <lo40ops@gmail.com>
2020-03-01 21:46:05 -0700

    Added file prologues

--- BattleAxe.Sms.Api/Controllers/ServiceStatusController.cs
+++ BattleAxe.Sms.Api/Controllers/ServiceStatusController.cs
@@ -1,4 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// ******************************************************************************************************************
+//  This file is part of BattleAxe.Sms.
+//
+//  BattleAxe.Sms - web service that handles all SMS communication.
+//  Copyright(C)  2020  James LoForti
+//  Contact Info: jamesloforti@gmail.com
+//
+//  BattleAxe.Sms is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+//									     ____.           .____             _____  _______   
+//									    |    |           |    |    ____   /  |  | \   _  \  
+//									    |    |   ______  |    |   /  _ \ /   |  |_/  /_\  \ 
+//									/\__|    |  /_____/  |    |__(  <_> )    ^   /\  \_/   \
+//									\________|           |_______ \____/\____   |  \_____  /
+//									                             \/          |__|        \/ 
+//
+// ******************************************************************************************************************
+//
+using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
 namespace BattleAxe.Sms.Api.Controllers
